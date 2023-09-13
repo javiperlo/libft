9b778d21069562582c6f4ce819f5f0ae6a8d8fd3
javiperlo <econectmail@gmail.com>
2023-09-13 18:57:25 +0200

    Cambios Strlcat Funcional

--- libtf/ft_strlcat.c
+++ libtf/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: javperez <javperez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 15:12:17 by javperez          #+#    #+#             */
-/*   Updated: 2023/09/13 16:13:23 by javperez         ###   ########.fr       */
+/*   Updated: 2023/09/13 18:57:10 by javperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,27 +27,24 @@ size_t	ft_strlcat(char *dst, char *src, size_t size)
 	}
 	while (src[j] != '\0' && j < size)
 	{
-		dst[i] = src[j];
+		dst[i + j] = src[j];
 		j ++;
 	}
-
-	for (size_t i = 0; i < sizeof(dst); i++)
-	{
-		printf ("%d", i);
-	}
-	
-	return (0);
+	dst[i + j] = '\0';
+	return (i + j);
 }
+/*
 int	main()
 {
 	char	src[] = "Javi";
 	char	dst[] = "Hola";
-	//int		len;
-
-	ft_strlcat(dst, src, 2);
-	//len = 3;
+	int		len = 1;
+	int		res;
+	
+	res = ft_strlcat(dst, src, len);
+	printf("Mi funcion saca este resultado: %d", res);
 	//printf("La funcion strlcat saca esto: %lu", strlcat(dst, src, len));
 }
-
+*/
 //Suma las dos cadenas "Javier" y "Hola" en dest y saca el numero de las dos cadenas juntas hasta 'l' numero.
 // En este caso si len = 3, sacaria 9. Javier + Hol
