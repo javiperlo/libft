8475ae6a0183f0ab9a9752c64b4dcec42d9c7e1b
Javier Perez Lopez <javperez@c2r18s5.42madrid.com>
2023-09-13 11:29:56 +0200

    Atoi Funcional

--- libtf/ft_atoi.c
+++ libtf/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: javperez <javperez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 19:22:46 by javperez          #+#    #+#             */
-/*   Updated: 2023/09/12 19:39:55 by javperez         ###   ########.fr       */
+/*   Updated: 2023/09/13 11:29:38 by javperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	ft_atoi(const char *str)
+int ft_atoi(const char *str)
 {
-	int		i;
-	int		sign;
-	int		res;
+	int i;
+	int sign;
+	int result;
 
 	i = 0;
-	res = 0;
 	sign = 1;
-	if (str[i] == ' ' || str[i] == '\t' || str[i] == '\r')
-		i ++;
-	/*
-	else if (str[i] == '+' || str[i] == '-')
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\f' || str[i] == '\r' || str[i] == '\v')
+		i++;
+	if (str[i] == '-')
 	{
-		if (str[i] == '+')
-			sign = 1;
-		else if (str[i] == '-')
-			sign = -1;
-		i ++;
-	}*/
-	else if (str[i] >= '0' && str[i] <= '9')
-	{
-		res = res * 10 + str[i] - '0';
-		i ++;
+		sign = -1;
+		i++;
 	}
-	return (res * sign);
-	
+	if (str[i] == '+')
+			i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i]  - '0');
+		i++;
+	}
+	return (result * sign);
 }
 
 int	main(void)
 {
-	char	str[] = "1234";
-	int res;
-	res = ft_atoi(str);
-	printf("%d",res);
-	//printf("MI ATOI\n");
-	//printf ("%d", ft_atoi(str));
+	char	str[] = "--1234";
+	printf("MI ATOI\n");
+	printf ("%d", ft_atoi(str));
 	printf("\nATOI ORIGINAL\n");
 	printf("%d", atoi(str));
 }
