2495a3242fa160ba8334af1a7105a6cf7019cc0f
Javier Perez Lopez <javperez@c2r17s5.42madrid.com>
2023-09-14 13:51:20 +0200

    Subido el strchr Terminado Funcional Norminette Corregida

--- libtf/ft_strchr.c
+++ libtf/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: javperez <javperez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 12:25:30 by javperez          #+#    #+#             */
-/*   Updated: 2023/09/14 13:18:21 by javperez         ###   ########.fr       */
+/*   Updated: 2023/09/14 13:50:11 by javperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,25 @@ char	*ft_strchr(char *str, int c)
 {
 	int		i;
 	int		j;
-	
+
 	i = 0;
 	j = 0;
-	while (str[i] != (char)c)
+	while (str[i] != '\0')
 	{	
+		if (str[i] == (char)c)
+		{
+			return (&str[i]);
+		}
 		i ++;
 	}
-	j = str[i];
-	return (0);
+	return (NULL);
 }
-
+/*
 int main(void)
 {
-	char	str[] = "hola mundo";
-	int		c = 'm';
-
-	//printf("Funcioón ORIGINAL: %s\n", strchr(str, c));
+	char	str[] = "";
+	int		c = 'd';
+	//ft_strchr(str, c);
+	printf("Funcioón ORIGINAL: %s\n", strchr(str, c));
 	printf("Función MIA: %s\n", ft_strchr(str, c));
-}
+}*/
