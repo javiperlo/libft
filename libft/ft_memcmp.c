10874998b3594ac4c20797e4688cc5cb36533edb
Javier Perez Lopez <javperez@c2r1s5.42madrid.com>
2023-09-15 14:24:05 +0200

    ft_memcmp Subido y pasado por Norminette

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: javperez <javperez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 12:23:14 by javperez          #+#    #+#             */
-/*   Updated: 2023/09/15 13:54:32 by javperez         ###   ########.fr       */
+/*   Updated: 2023/09/15 14:23:29 by javperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,30 @@
 
 int	ft_memcmp(const void *str1, const void *str2, size_t n)
 {
-    size_t      i;
+	size_t		i;
 
-    i = 0;
-    while ((char *))
-    {
-        /* code */
-    }
-    
+	i = 0;
+	while (((char *)str1)[i] != '\0' && ((char *)str2)[i] != '\0' && i < n)
+	{
+		if (((char *)str1)[i] == ((char *)str2)[i])
+		{
+			i ++;
+		}
+		else
+			return (((char *)str1)[i] - ((char *)str2)[i]);
+	}
+	return (0);
 }
-
+/*
 int main()
 {
-    char str1[] = "Hola";
-    char str2[] = "HOla";
-    int n;
-    int puntero;
+	char str1[] = " h";
+	char str2[] = "<H";
+	int n;
+	int puntero;
 
-    n = 1;
-    puntero =  memcmp(str1, str2, n); // Devuelve 0 si son iguales, 1 si str1 es mayor que str2 y -1 si str1 es menor que str2
-    printf("%d", puntero); //Al almacenar memcmp en una variable int y luego imprimirlo, se imprime el valor de retorno de memcmp
-}
+	n = 2;
+	printf("La función ORIGINAL: %d\n", memcmp(str1, str2, n));
+	puntero =  ft_memcmp(str1, str2, n);
+	printf("La función MIA: %d\n", puntero); 
+}*/
