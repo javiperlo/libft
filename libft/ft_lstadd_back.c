bf4124e157605d9824e4e128e4d26e898b267927
javiperlo <econectmail@gmail.com>
2023-09-24 21:56:51 +0200

    Cambios 24 Sept, BONUS Actualizado noche

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -1,39 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: javperez <javperez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/24 13:03:11 by javperez          #+#    #+#             */
-/*   Updated: 2023/09/24 21:56:06 by javperez         ###   ########.fr       */
+/*   Created: 2023/09/24 21:05:15 by javperez          #+#    #+#             */
+/*   Updated: 2023/09/24 21:37:20 by javperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_lstsize(t_list *lst)
-{
-	int		i;
+/**
+ * Descripción: Añade el nodo 'new' al final de la lista 'lst'
+*/
 
-	i = 0;
-	while (lst != NULL)
-	{
-		lst = lst -> next;
-		i ++;
-	}
-	return (i);
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
 }
-/*
+
 int	main(void)
 {
-	int		i;
 	
-	t_list *lst = ft_lstnew((int *)42);
-    lst->next = ft_lstnew((int *)24);
-    lst->next -> next = ft_lstnew((int *)24);
-    lst->next->next -> next = NULL;
-	i = ft_lstsize(lst);
-	printf("El tamaño de la lista es: %d\n", i);
-	return (0);
-}*/
+}	
